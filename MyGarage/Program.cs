7cd4cb9abb0c5b57e7dddabb5683cd467c80f339
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-09-09 18:36:23 -0500

    Add project files.

---
+++ MyGarage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyGarage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
